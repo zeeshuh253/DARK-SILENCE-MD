45eedfe4e9fe259789f491bcdf011047e3bb9507
《●SILENT ♡LOVER⁴³²●》 <burhanking632@gmail.com>
2025-05-28 02:12:29 +0500

    Update SILENT-SOBX-MD66.js

--- plugins/SILENT-SOBX-MD66.js
+++ plugins/SILENT-SOBX-MD66.js
@@ -12,60 +12,23 @@ cmd({
     // Cheack is owner and bot number
     if (!isOwner && senderNumber !== botNumber) return reply('This command is only for the bot owner and bot number.');
 
-    // Check if there is a quoted message and if it's a ViewOnce message
-    const viewOnceData = mek.msg.contextInfo?.quotedMessage?.viewOnceMessageV2;
-    if (viewOnceData) {
-      // If the quoted message is an image
-      if (viewOnceData.message.imageMessage) {
-        console.log("Quoting an image");
-        let caption = viewOnceData.message.imageMessage.caption;
-        let savedPath = await conn.downloadAndSaveMediaMessage(viewOnceData.message.imageMessage);
-        return conn.sendMessage(mek.chat, { image: { url: savedPath }, caption: caption });
+    if (!quoted) return reply("Please reply to a ViewOnce message");
+
+    if (quoted.mtype === "viewOnceMessageV2") {
+      let mediaType = Object.keys(quoted.message.viewOnceMessageV2.message)[0];
+      let savedPath = await conn.downloadAndSaveMediaMessage(quoted.message.viewOnceMessageV2.message[mediaType]);
+      
+      if (mediaType === "imageMessage") {
+        return conn.sendMessage(mek.chat, { image: { url: savedPath }, caption: quoted.message.viewOnceMessageV2.message[mediaType].caption });
+      } else if (mediaType === "videoMessage") {
+        return conn.sendMessage(mek.chat, { video: { url: savedPath }, caption: quoted.message.viewOnceMessageV2.message[mediaType].caption });
+      } else if (mediaType === "audioMessage") {
+        return conn.sendMessage(mek.chat, { audio: { url: savedPath }});
       }
-      // If the quoted message is a video
-      else if (viewOnceData.message.videoMessage) {
-        let caption = viewOnceData.message.videoMessage.caption;
-        let savedPath = await conn.downloadAndSaveMediaMessage(viewOnceData.message.videoMessage);
-        return conn.sendMessage(mek.chat, { video: { url: savedPath }, caption: caption });
-      }
-      // If the quoted message is a voice note
-      else if (viewOnceData.message.audioMessage) {
-        let caption = viewOnceData.message.audioMessage;
-        let savedPath = await conn.downloadAndSaveMediaMessage(viewOnceData.message.audioMessage);
-        return conn.sendMessage(mek.chat, { audio: { url: savedPath }, });
-      }
+    } else {
+      return reply("This is not a ViewOnce message");
     }
   } catch (error) {
     console.log("Error processing viewOnce message:", error);
-  }
-
-  // Handle when the quoted message is not a ViewOnce message
-  if (!quoted) {
-    return reply("Please reply to a ViewOnce message");
-  }
-
-  // Check if the quoted message is a ViewOnce message
-  if (quoted.mtype === "viewOnceMessage") {
-    console.log("Processing a ViewOnce message");
-    // If the ViewOnce message is an image
-    if (quoted.message.imageMessage) {
-      let caption = quoted.message.imageMessage.caption;
-      let savedPath = await conn.downloadAndSaveMediaMessage(quoted.message.imageMessage);
-      return conn.sendMessage(mek.chat, { image: { url: savedPath }, caption: caption });
-    }
-    // If the ViewOnce message is a video
-    else if (quoted.message.videoMessage) {
-      let caption = quoted.message.videoMessage.caption;
-      let savedPath = await conn.downloadAndSaveMediaMessage(quoted.message.videoMessage);
-      return conn.sendMessage(mek.chat, { video: { url: savedPath }, caption: caption });
-    }
-    // If the ViewOnce message is a voice note
-    else if (quoted.message.audioMessage) {
-      let caption = quoted.message.audioMessage.caption;
-      let savedPath = await conn.downloadAndSaveMediaMessage(quoted.message.audioMessage);
-      return conn.sendMessage(mek.chat, { audio: { url: savedPath }, caption: caption });
-    }
-  } else {
-    return reply("This is not a ViewOnce message");
   }
 });
