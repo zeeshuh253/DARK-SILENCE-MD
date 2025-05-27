e071ce9d254ff3d584da5779e4b98ac9bab66ae1
《●SILENT ♡LOVER⁴³²●》 <burhanking632@gmail.com>
2025-05-28 01:52:36 +0500

    Create SILENT-SOBX-MD66.js

---
+++ plugins/SILENT-SOBX-MD66.js
@@ -0,0 +1,91 @@
+const {
+    default: makeWASocket,
+    getAggregateVotesInPollMessage, 
+    useMultiFileAuthState,
+    DisconnectReason,
+    getDevice,
+    fetchLatestBaileysVersion,
+    jidNormalizedUser,
+    getContentType,
+    Browsers,
+    delay,
+    makeInMemoryStore,
+    makeCacheableSignalKeyStore,
+    downloadContentFromMessage,
+    generateForwardMessageContent,
+    generateWAMessageFromContent,
+    prepareWAMessageMedia,
+    proto
+} = require('@whiskeysockets/baileys')
+const fs = require('fs')
+const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson} = require('../lib/functions')
+const axios = require('axios')
+const FileType = require('file-type')
+const config = require('../config');
+const {cmd , commands} = require('../command')
+
+cmd({
+  pattern: "vv3",
+  react: "😁",
+  alias: ["lassanai", "patta", "mekada", "supiri", "maru"],
+  desc: "To ViewOnceMessage",
+  category: "convert",
+  use: '.vv',
+  filename: __filename
+}, async (conn, mek, m, { from, prefix, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+  try {
+    conn.downloadAndSaveMediaMessage = async (message, filename, attachExtension = true) => {
+      let quoted = message.msg ? message.msg : message
+      let mime = (message.msg || message).mimetype || ''
+      let messageType = message.mtype ? message.mtype.replace(/Message/gi, '') : mime.split('/')[0]
+      if (messageType === 'ptt') messageType = 'audio'
+      const stream = await downloadContentFromMessage(quoted, messageType)
+      let buffer = Buffer.from([])
+      for await (const chunk of stream) {
+        buffer = Buffer.concat([buffer, chunk])
+      }
+      let type = await FileType.fromBuffer(buffer)
+      trueFileName = attachExtension ? (filename + '.' + type.ext) : filename
+      await fs.writeFileSync(trueFileName, buffer)
+      return trueFileName
+    }
+
+    const quot = mek.msg.contextInfo.quotedMessage.viewOnceMessageV2;
+    if (quot) {
+      if (quot.message.imageMessage) {
+        let cap = quot.message.imageMessage.caption;
+        let anu = await conn.downloadAndSaveMediaMessage(quot.message.imageMessage)
+        return conn.sendMessage(from, { image: { url: anu }, caption: cap })
+      }
+      if (quot.message.videoMessage) {
+        let cap = quot.message.videoMessage.caption;
+        let anu = await conn.downloadAndSaveMediaMessage(quot.message.videoMessage)
+        return conn.sendMessage(from, { video: { url: anu }, caption: cap })
+      }
+      if (quot.message.audioMessage || quot.message.pttMessage) {
+        let anu = await conn.downloadAndSaveMediaMessage(quot.message.audioMessage || quot.message.pttMessage)
+        return conn.sendMessage(from, { audio: { url: anu }, mimetype: 'audio/mpeg' })
+      }
+    }
+
+    if (!mek.quoted) return mek.reply("```Uh Please Reply A ViewOnce Message```")
+    if (mek.quoted.mtype === "viewOnceMessage") {
+      if (mek.quoted.message.imageMessage) {
+        let cap = mek.quoted.message.imageMessage.caption;
+        let anu = await conn.downloadAndSaveMediaMessage(mek.quoted.message.imageMessage)
+        conn.sendMessage(from, { image: { url: anu }, caption: cap })
+      } else if (mek.quoted.message.videoMessage) {
+        let cap = mek.quoted.message.videoMessage.caption;
+        let anu = await conn.downloadAndSaveMediaMessage(mek.quoted.message.videoMessage)
+        conn.sendMessage(from, { video: { url: anu }, caption: cap })
+      } else if (mek.quoted.message.audioMessage || mek.quoted.message.pttMessage) {
+        let anu = await conn.downloadAndSaveMediaMessage(mek.quoted.message.audioMessage || mek.quoted.message.pttMessage)
+        conn.sendMessage(from, { audio: { url: anu }, mimetype: 'audio/mpeg' })
+      }
+    } else return mek.reply("```This is Not A ViewOnce Message```")
+    await conn.sendMessage(from, { react: { text: `✅`, key: mek.key } })
+  } catch (e) {
+    console.log(e)
+    reply(`*please try again⭕*`)
+  }
+})
